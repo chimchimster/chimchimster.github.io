f10c01fe209af3c52296fa916af4e4d6ab816c98
chimchimster <jorddyishere@gmail.com>
2023-07-01 10:45:46 +0600

    changed css a little bit + started requests in js

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 });
+
+function createRequest() {
+    let Request = false;
+    if (window.XMLHttpRequest) {
+        Request = new XMLHttpRequest();
+    } else if {
+        try {
+            Request = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (CatchException) {
+            Request = new ActiveXObject('Msxml.XMLHTTP');
+        }
+    }
+
+    if (!Request) {
+        alert('Can not create HTTP Request');
+    }
+
+    return Request;
+}
+
+function sendRequest(method, path, args, r_handler) {
+    let Request = createRequest();
+
+    if (!Request) {
+        return;
+    }
+
+    Request.onreadystatechange = () => {
+        if (Request.readystate == 4) {
+            r_handler(Request);
+        }
+
+    }
+}
+
